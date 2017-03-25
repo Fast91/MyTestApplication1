1c81a9b77357a6363e0124a5b60feb4866fe7f7c
thekooks94 <roberto.spaz@virgilio.it>
2017-03-25 10:44:08 +0100

    lbalabl

--- app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
+++ app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.faust.mytestapplication1;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1 = (Button) findViewById(R.id.b1);
+        Typeface font = Typeface.createFromAsset(getAssets(), "GoodDog.otf");
+        b1.setTypeface(font);
+
     }
 }
