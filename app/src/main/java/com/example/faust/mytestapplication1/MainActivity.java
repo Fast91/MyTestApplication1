a729567e1183ccb2df81714a47b33b35b80270e6
thekooks94 <roberto.spaz@virgilio.it>
2017-03-25 12:08:00 +0100

    lbalabl

--- app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
+++ app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b1 = (Button) findViewById(R.id.b1);
-        Typeface font = Typeface.createFromAsset(getAssets(), "Amatic-Bold.ttf");
-        b1.setTypeface(font);
+       /* Button b1 = (Button) findViewById(R.id.b1);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Amatic-Bold.ttf");
+        b1.setTypeface(font); */
+
+
 
     }
 }
