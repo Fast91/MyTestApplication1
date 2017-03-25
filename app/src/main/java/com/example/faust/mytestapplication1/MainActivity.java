9adab16646906e78e85826d38d1d86c2fbeda18a
Fast91 <faustosavarino@gmail.com>
2017-03-25 10:54:31 +0100

    appcompatibility + toolbar

--- app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
+++ app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.faust.mytestapplication1;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
