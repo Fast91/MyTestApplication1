43ebfdd5ee4cac2b943ed0676aecef05468e6868
thekooks94 <roberto.spaz@virgilio.it>
2017-03-25 12:16:19 +0100

    lbalabl

--- app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
+++ app/src/main/java/com/example/faust/mytestapplication1/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,7 +18,43 @@ public class MainActivity extends AppCompatActivity {
        /* Button b1 = (Button) findViewById(R.id.b1);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Amatic-Bold.ttf");
         b1.setTypeface(font); */
+        final Button b1 = (Button) findViewById(R.id.b1);
+        final Button b2 = (Button) findViewById(R.id.b2);
+        final Button b3 = (Button) findViewById(R.id.b3);
 
+        b1.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                b1.setPressed(true);
+                b2.setPressed(false);
+                b3.setPressed(false);
+                return true;
+            }
+        });
+
+
+        b2.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                b1.setPressed(false);
+                b3.setPressed(false);
+                b2.setPressed(true);
+                return true;
+            }
+        });
+
+        b3.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                b3.setPressed(true);
+                b1.setPressed(false);
+                b2.setPressed(false);
+                return true;
+            }
+        });
 
 
     }
