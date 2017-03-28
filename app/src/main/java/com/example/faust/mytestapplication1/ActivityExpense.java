73009e0c19108d2b7a8957e3c9b212c37b8f7023
thekooks94 <roberto.spaz@virgilio.it>
2017-03-28 20:50:57 +0200

    Toolbar + MENU - Groups By Rob

--- app/src/main/java/com/example/faust/mytestapplication1/ActivityExpense.java
+++ app/src/main/java/com/example/faust/mytestapplication1/ActivityExpense.java
@@ -6,7 +6,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
-public class Activity2Test extends AppCompatActivity {
+public class ActivityExpense extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
