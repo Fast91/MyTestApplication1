cab9dd5139f4f554a0eb1809f8cac482fda795dd
thekooks94 <roberto.spaz@virgilio.it>
2017-04-02 16:46:53 +0200

    BACKBUTTON - Groups By Rob

--- app/src/main/java/com/example/faust/mytestapplication1/LoginActivity.java
+++ app/src/main/java/com/example/faust/mytestapplication1/LoginActivity.java
@@ -1,13 +1,153 @@
 package com.example.faust.mytestapplication1;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //defining views
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignup;
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //getting firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the objects getcurrentuser method is not null
+        //means user is already logged in
+        if(firebaseAuth.getCurrentUser() != null){
+            //close this activity
+            finish();
+            //opening profile activity
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        //initializing views
+        editTextEmail = (EditText) findViewById(R.id.email_login);
+        editTextPassword = (EditText) findViewById(R.id.password_login);
+        buttonSignIn = (Button) findViewById(R.id.email_login_button);
+        textViewSignup  = (TextView) findViewById(R.id.textview_notregistered);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching click listener
+        buttonSignIn.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
     }
+
+
+
+    //method for user login
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+
+
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,R.string.enter_email,Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,R.string.enter_password,Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        String s=(String)getString(R.string.progress_bar_login);
+        progressDialog.setMessage(s);
+        progressDialog.show();
+
+
+
+        //logging in the user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                        progressDialog.dismiss();
+                        //if the task is successfull
+                        if(task.isSuccessful()){
+                            //start the profile activity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,R.string.wrong_login,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonSignIn){
+            userLogin();
+        }
+
+        if(view == textViewSignup){
+            finish();
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.backmain_title)
+                .setMessage(R.string.backmain_message)
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        //intent.putExtra("ID_USER",firebaseAuth.getCurrentUser().getUid());
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                }).create().show();
+    }
+
 }
--- app/src/main/java/com/example/faust/mytestapplication1/RegisterActivity.java
+++ app/src/main/java/com/example/faust/mytestapplication1/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.faust.mytestapplication1;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -27,6 +30,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private View mLoginFormView;
     private ProgressDialog myprogressBar;
     private FirebaseAuth firebaseauth;
+
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -56,6 +60,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         }
         if(v==mLoginFormView){
+            startActivity(new Intent(this, LoginActivity.class));
 
 
         }
@@ -78,9 +83,11 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             Toast.makeText(this,R.string.enter_password,Toast.LENGTH_SHORT).show();
             return;
         }
-        String s=(String)getString(R.string.progress_bar);
+
+        String s=(String)getString(R.string.progress_bar_register);
         myprogressBar.setMessage(s);
         myprogressBar.show();
+
         firebaseauth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -89,6 +96,9 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
                     Toast.makeText(RegisterActivity.this,R.string.successful_registration,Toast.LENGTH_SHORT).show();
 
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
                 }
                 else{
 
@@ -96,10 +106,35 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
 
                 }
+                myprogressBar.dismiss();
 
             }
         });
 
     }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.backmain_title)
+                .setMessage(R.string.backmain_message)
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        //intent.putExtra("ID_USER",firebaseAuth.getCurrentUser().getUid());
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        //finish();
+
+                    }
+                }).create().show();
+    }
+
 }
     
