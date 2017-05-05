d9b6b63b41417f6b7c75fa07a9b0e7ac2e48d1de
Fast91 <faustosavarino@gmail.com>
2017-05-05 18:12:07 +0200

    db balances - #import inutili

--- app/src/main/java/com/example/faust/mytestapplication1/DBShortKeys.java
+++ app/src/main/java/com/example/faust/mytestapplication1/DBShortKeys.java
@@ -1,15 +1,10 @@
 package com.example.faust.mytestapplication1;
 
-import android.provider.ContactsContract;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by faust on 05/05/2017.
